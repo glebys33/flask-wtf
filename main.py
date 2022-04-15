a39befd3bb5ead1f6d668ee7ab08852e9b44f8c9
glebys33 <glebys.kiselev@gmail.com>
2022-04-15 15:33:54 +0300

    Тренировки в полёте

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -12,5 +12,14 @@ def index(title):
     return render_template('base.html', **param)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    param = {}
+    param['prof'] = prof
+    param['url1'] = url_for('static', filename='img/1.jpg')
+    param['url2'] = url_for('static', filename='img/2.jpg')
+    return render_template('specialty.html', **param)
+
+
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=8000, host='127.0.0.1')
